c233615331f7b6b4183e6e48e6998ae0f67562ea
Jerry Okafor <jerryhanksokafor@gmail.com>
2024-06-21 08:48:15 -0600

    fx release checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "ComposeApp",
-         url: "https://github.com/jerryOkafor/kmp-spm-framework/archive/refs/tags/v0.0.1.zip",
+         url: "https://github.com/jerryOkafor/kmp-spm-framework/releases/download/v0.0.2/ComposeApp.xcframework.zip",
          checksum:"6509e215b4438668029da40cbbd5b2a0bd3c3c00859104d6f5d511475e3a8a5e")
    ]
 )
